e202f7b33b49781b4091dcd3e177fb0b410c2efd
Harley Montgomery <harleymontgomery@gmail.com>
2012-10-14 22:50:06 -0700

    Added preset symbols

--- Parser.hs
+++ Parser.hs
@@ -168,7 +168,16 @@ symbolHelper (CCommand d c j:rest) lineNum table vars =
 
 -- The preset symbols, not done yet
 presetSymbols :: SymbolTable
-presetSymbols = M.empty 
+presetSymbols = M.fromList [("SP",0),
+	      		    ("LCL",1),
+			    ("ARG",2),
+			    ("THIS",3),
+			    ("THAT",4),
+			    ("R0",0),("R1",1),("R2",2),("R3",3),
+			    ("R4",4),("R5",5),("R6",6),("R7",7),
+			    ("R8",8),("R9",9),("R10",10),("R11",11),
+			    ("R12",12),("R13",13),("R14",14),("R15",15),
+			    ("SCREEN",16384),("KBD",24576)]
 
 -- Turns a command list into a symbol table, including the presets
 getSymbols :: [Command] -> SymbolTable
